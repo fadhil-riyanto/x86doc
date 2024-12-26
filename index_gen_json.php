eb5d31700dd79d7d5eebd317aa05119d15abd2fe
fadhil riyanto <me@fadev.org>
2024-12-26 22:03:50 +0700

    generate json instead HTML

---
+++ index_gen_json.php
@@ -0,0 +1,23 @@
+<?php 
+function append($txt) {
+	file_put_contents('./html/index.html', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
+}
+
+$data = array_diff(scandir("./html"), array('.', '..', 'script.js', 'style.css', 'data.json'));
+
+$packed = [];
+foreach($data as $key => $data_s ) {
+	$full_noraw = [
+		"keyword" => explode("_", explode(".", $data_s)[0]),
+		"file" => $data_s
+	];
+	array_push($packed, $full_noraw);
+}
+
+
+file_put_contents("./html/data.json", json_encode([
+	"data" => $packed
+], JSON_PRETTY_PRINT));
+
+
+// var_dump($packed);
