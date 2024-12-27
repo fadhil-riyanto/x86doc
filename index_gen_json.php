56783051cc564e74af6110664fb12808e17dcbfe
fadhil riyanto <me@fadev.org>
2024-12-27 07:41:52 +0700

    fix error comma separated data

--- index_gen_json.php
+++ index_gen_json.php
@@ -7,9 +7,12 @@ $data = array_diff(scandir("./html"), array('.', '..', 'script.js', 'style.css',
 
 $packed = [];
 foreach($data as $key => $data_s ) {
+
+	$exp_ = explode("_", explode(".", str_replace(",", "_", $data_s))[0]);
+	
 	$full_noraw = [
 		"title" => explode(".", $data_s)[0],
-		"keyword" => explode("_", explode(".", $data_s)[0]),
+		"keyword" => $exp_,
 		"file" => $data_s
 	];
 	array_push($packed, $full_noraw);
